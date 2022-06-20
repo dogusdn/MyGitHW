db83cff83739318d2bec9bc5b1e6c8a7709b3c88
DOHYEONWOO <dogusdn@gmail.com>
2022-06-20 17:02:54 +0900

    myFirst Commit!

---
+++ MyGitTry2/src/B.java
@@ -0,0 +1,6 @@
+
+public class B {
+	public void sayHello() {
+		System.out.println("Hello This is B.java File");
+	}
+}
---
+++ MyGitTry2/src/C.java
@@ -0,0 +1,6 @@
+
+public class C {
+	public void sayHello() {
+		System.out.println("Hello This is C.java File");
+	}
+}
